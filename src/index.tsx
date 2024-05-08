069f61f768f1ef96ab01cb7d369bf52622a33d25
HVMS <viralsid2330@gmail.com>
2024-05-08 00:41:07 -0300

    chore: Wrap App component with ChakraProvider for Chakra UI integration

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ChakraProvider } from '@chakra-ui/react';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    {/***  Wrap the App component with the ChakraProvider to enable the Chakra UI */}
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
 
