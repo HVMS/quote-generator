8fea685ed84621f2aa2d122c1e1db34eef62b1d9
HVMS <viralsid2330@gmail.com>
2024-05-09 02:49:24 -0300

    chore: Update QuoteInput component to fetch quotes from API based on user input

--- src/components/Quotes/QuoteInput.tsx
+++ src/components/Quotes/QuoteInput.tsx
@@ -1,7 +1,41 @@
-import { Box, Button, Input, InputGroup, InputRightElement, Stack } from "@chakra-ui/react";
-import { EmailIcon, Search2Icon } from "@chakra-ui/icons";
+import { Box, Button, Input, InputGroup, InputRightElement, Stack, background } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 
 export default function QuoteInput() {
+
+    const [searchQuote, setSearchQuote] = useState("");
+    const [showQuote, setShowQuote] = useState(false);
+
+    const[quoteResult, setQuoteResult] = useState("");
+
+    useEffect( () => {
+        if (searchQuote.length > 0 && searchQuote !== null) {
+            return ;
+        }
+
+        const quote = async () => {
+            try {
+                const response = await fetch(`https://api.api-ninjas.com/v1/quotes?category=${searchQuote}`, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'X-Api-Key': 'nI9QPk4qicX35si/Jk2XAA==O3lgXnSDBrMMsbhX',
+                    },
+                });
+                
+                const data = await response.json();
+                console.log("Quote data: ", data);
+                setShowQuote(true);
+                setQuoteResult(data.quote);
+                console.log("Quote result: ", quoteResult);
+
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+    },[]);
+
     return (
         <div>
             <Box p={{ base: 2, md: 10 }} borderColor={"gray"}>
@@ -9,13 +43,28 @@ export default function QuoteInput() {
                     <Input
                         placeholder="Enter your topic to generate quotes quickly using AI"
                         size={{ base: "md", md: "lg" }}
+                        onChange={e => setSearchQuote(e.target.value)}
                     />
                     <InputRightElement width={{ base: "4.5rem", md: "7.5rem" }} m={1}>
-                        <Button h={{ base: "1.75rem", md: "2.5rem" }} size={{ base: "md", md: "lg" }} leftIcon={<Search2Icon />} bg={"#FFD351"}>
+                        <Button
+                            h={{ base: "1.75rem", md: "2.5rem" }}
+                            size={{ base: "md", md: "lg" }}
+                            bg={"#FFD351"}
+                            _hover={{ bg: "#F8A819" }}
+                        >
                             Generate
                         </Button>
                     </InputRightElement>
                 </InputGroup>
+            </Box>
+
+            <Box>
+                {/* Only show text if showQuote is true */}
+                {showQuote && <Stack>
+                    <Box>
+                        <p>{quoteResult}</p>
+                    </Box>
+                </Stack>}
             </Box>
         </div>
     );
