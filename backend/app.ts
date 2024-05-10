72395034b4c2c319952f69590e1934da7026c4db
HVMS <viralsid2330@gmail.com>
2024-05-10 01:53:55 -0300

    feat: Add getQuoteByCategoryRouter for handling quote category requests

--- backend/app.ts
+++ backend/app.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 
+import { getQuoteByCategoryRouter } from './controller/getQuoteByCategory';
+
 // Create an Express app
 const app = express();
 
@@ -11,13 +13,13 @@ app.use(cors());
 // Middleware to parse JSON in request body
 app.use(express.json());
 
-
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello from express typescript');
+    res.send('Hello from social media quote generator');
 });
 
-app.use('/quotes', require('./routes/quotes'));
+// Mount the router at /quote
+app.use('/quotes', getQuoteByCategoryRouter);
 
-app.listen(3000, () => {
-    console.log('Server started on port 3000');
+app.listen(3001, () => {
+    console.log('Server started on port 3001');
 });
