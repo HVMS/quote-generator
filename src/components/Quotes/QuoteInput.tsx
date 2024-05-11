565994c681fac5f75c02d5cabe8764696c842124
HVMS <viralsid2330@gmail.com>
2024-05-10 22:26:29 -0300

    chore: Refactor QuoteInput component to improve code structure and remove unused variables

--- src/components/Quotes/QuoteInput.tsx
+++ src/components/Quotes/QuoteInput.tsx
@@ -1,85 +1,59 @@
-import { Box, Button, Card, CardBody, CardHeader, Heading, Input, InputGroup, InputRightElement, Stack, background } from "@chakra-ui/react";
-import React from "react";
-import { useState } from "react";
+import { Box, Button, Card, CardBody, CardFooter } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 import "../../styles/quote.css";
 import envVariables from "../../importenv";
+import React from "react";
 
 export default function QuoteInput() {
 
-    const [seachQuoteCategory, setSearchQuoteCategory] = useState("");
-    const [showQuote, setShowQuote] = useState(false);
-
     const [quoteResult, setQuoteResult] = useState("");
 
+    const shareQuoteOnTweet = () => {
+        const twitterURL = `https://twitter.com/intent/tweet?text=${quoteResult}`;
+        window.open(twitterURL, '_blank');
+    };
+
+    useEffect(() => {
+        onGenerateQuote();
+    }, []);
+
     const onGenerateQuote = async () => {
-        if (seachQuoteCategory === "") {
-            alert("Please enter a topic to generate a quote");
-            return;
-        }
-
-        // Fetch the quote which expects an array of properties
-
-        const quote: any = await processQuote(seachQuoteCategory);
+        const quote: any = await processQuote();
         if (quote) {
-            setQuoteResult(quote);
-            setShowQuote(true);
+            setQuoteResult(quote.content);
         } else {
             setQuoteResult("No quote found for this topic. Please try again.");
-            setShowQuote(true);
         }
     }
 
     return (
-        <div>
-            <Box p={{ base: 2, md: 10 }} borderColor={"gray"}>
-                <InputGroup>
-                    <Input
-                        placeholder="Enter your topic to generate quotes quickly using AI"
-                        size={{ base: "md", md: "lg" }}
-                        onChange={e => setSearchQuoteCategory(e.target.value)}
-                    />
-                    <InputRightElement width={{ base: "4.5rem", md: "7.5rem" }} m={1}>
-                        <Button
-                            h={{ base: "1.75rem", md: "2.5rem" }}
-                            size={{ base: "md", md: "lg" }}
-                            bg={"#FFD351"}
-                            _hover={{ bg: "#F8A819" }}
-                            onClick={onGenerateQuote}
-                        >
-                            Generate
-                        </Button>
-                    </InputRightElement>
-                </InputGroup>
-            </Box>
-            <div>
-                {showQuote && <Stack>
-                    <Card className="card">
-                        <CardHeader>
-                            <Heading size='md'>Quote of the Day</Heading>
-                        </CardHeader>
-                        <CardBody>
-                            <Box>
-                                <Box className="quoteBox">
-                                    <p className="quoteResult">{quoteResult}</p>
-                                </Box>
-                            </Box>
-                        </CardBody>
-                    </Card>
-                </Stack>}
-
-            </div>
+        <div className="cardCSS">
+            <Card>
+                <CardBody>
+                    <Box>
+                        <p className="quoteResultCSS">{quoteResult ? quoteResult : "No quote available"}</p>
+                    </Box>
+                </CardBody>
+                <CardFooter
+                    justifyContent={"space-between"}
+                    flexWrap={"wrap"}
+                >
+                    <Button onClick={() => shareQuoteOnTweet()}>Twitter</Button>
+                    <Button onClick={() => onGenerateQuote()}>Generate Quote</Button>
+                </CardFooter>
+            </Card>
         </div>
     );
 };
 
-async function processQuote(seachQuoteCategory: string): Promise<string> {
+async function processQuote(): Promise<string> {
 
     const backendURL = envVariables.backendURL;
     console.log("Backend URL: ", backendURL);
 
     try {
 
-        const response = await fetch(`${backendURL}/quotes?category=${seachQuoteCategory}`, {
+        const response = await fetch(`${backendURL}/quotes/random`, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json'
