73342d5055cfced66d93e8fe09f962c0354b618e
HVMS <viralsid2330@gmail.com>
2024-05-10 22:26:01 -0300

    feat: Update importenv.ts to use hardcoded backend URL

--- src/importenv.ts
+++ src/importenv.ts
@@ -1,7 +1,4 @@
-// Author: Harshil Shah
 const variables = {'backendURL': 'http://127.0.0.1:3001'};
-// const variables = {'backendURL': 'http://localhost:3000', 'frontendURL': 'https://classmate-g7.netlify.app'};
-// testing gitignore
 const entries = Object.entries(variables);
 const envVariables: Record<string, string> = {};
 
